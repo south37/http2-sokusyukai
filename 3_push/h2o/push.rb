c0e5656b06c74e30ab18d78fec2b3556b74cdba0
Nao Minami <south37777@gmail.com>
2016-10-13 16:00:16 +0900

    Add images to push

--- 3_push/h2o/push.rb
+++ 3_push/h2o/push.rb
@@ -2,6 +2,9 @@ Proc.new do |env|
   push_paths = []
   if /(\/|\.html)\z/.match(env["PATH_INFO"])
     push_paths << "/css/application.css"
+    180.times do |i|
+      push_paths << "/images/photo#{i + 1}.jpg"
+    end
   end
   [399, push_paths.empty? ? {} : {"link" => push_paths.map{|p| "<#{p}>; rel=preload"}.join("\n")}, []]
 end
